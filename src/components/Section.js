0dd8a28152b90c16e5d7ce7f217c78d816c30b54
Jessica Garcia <jessie04ka@gmail.com>
2025-08-26 20:04:13 -0500

    project 9 additions

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,11 +4,19 @@ export default class Section {
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
+  setItems(items) {
+    this._items = items;
+    this.renderItems();
+  }
+
   renderItems() {
+    this._container.innerHTML = "";
     this._items.forEach((item) => {
-      this._renderer(item);
+      const element = this._renderer(item);
+      this.addItem(element);
     });
   }
+
   addItem(element) {
     this._container.prepend(element);
   }
