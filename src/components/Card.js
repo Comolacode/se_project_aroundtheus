8be2fe99917a8261bb76af00b6c5d2bf7a760bb4
Jessica Garcia <jessie04ka@gmail.com>
2025-08-26 20:04:13 -0500

    project 9 additions

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,15 @@
 export default class Card {
-  constructor(data, cardSelector, handleImageClick) {
+  constructor(data, cardSelector, handleImageClick, handleDeleteClick) {
     this._name = data.name;
     this._link = data.link;
+    this._id = data._id;
+    this._ownerId = data.owner ? data.owner._id : null;
+    this._currentUserId = data.currentUserId;
     this._cardSelector = cardSelector;
     this._handleImageClick = handleImageClick;
+    this._handleDeleteClick = data.handleDeleteClick || (() => {});
   }
+
   _getTemplate() {
     const cardElement = document
       .querySelector(this._cardSelector)
@@ -12,27 +17,13 @@ export default class Card {
       .cloneNode(true);
     return cardElement;
   }
-  _setEventListeners() {
-    this._likeButton.addEventListener("click", () => {
-      this._likeButton.classList.toggle("card__like-button_active");
-    });
-    this._deleteButton.addEventListener("click", () => {
-      this._cardElement.remove();
-    });
-
-    this._imageElement.addEventListener("click", () => {
-      this._handleImageClick({ name: this._name, link: this._link });
-    });
-  }
 
   generateCard() {
-    this._cardElement = this._getTemplate();
-    this._imageElement = this._cardElement.querySelector(".card__image");
-    this._likeButton = this._cardElement.querySelector(".card__like-button");
-    this._deleteButton = this._cardElement.querySelector(
-      ".card__delete-button"
-    );
-    this._titleElement = this._cardElement.querySelector(".card__title");
+    this._element = this._getTemplate();
+    this._imageElement = this._element.querySelector(".card__image");
+    this._likeButton = this._element.querySelector(".card__like-button");
+    this._deleteButton = this._element.querySelector(".card__delete-button");
+    this._titleElement = this._element.querySelector(".card__title");
 
     this._imageElement.src = this._link;
     this._imageElement.alt = this._name;
@@ -40,6 +31,18 @@ export default class Card {
 
     this._setEventListeners();
 
-    return this._cardElement;
+    return this._element;
+  }
+  _setEventListeners() {
+    this._likeButton.addEventListener("click", () => {
+      this._likeButton.classList.toggle("card__like-button_active");
+    });
+    this._deleteButton.addEventListener("click", () => {
+      this._handleDeleteClick(this._element, this._id);
+    });
+
+    this._imageElement.addEventListener("click", () => {
+      this._handleImageClick({ name: this._name, link: this._link });
+    });
   }
 }
